975d355267086506ad2330a05d785c9889e38b60
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-14 00:11:05 +0000

    Parse through world control functions through Taconite

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,4 @@
-use ecs_rust::world::World;
+use ecs_rust::{component::Component, system::System, world::World};
 
 pub trait EventHandler {
     fn update(&mut self);
@@ -24,5 +24,34 @@ impl Default for Taconite {
         Self {
             world: World::new(),
         }
+    }
+}
+
+impl Taconite {
+    pub fn create_entity(&mut self) -> usize {
+        self.world.create_entity()
+    }
+
+    pub fn remove_entity(&mut self, entity_id: usize) {
+        self.world.remove_entity(entity_id);
+    }
+
+    pub fn register_component<T: 'static + Component>(&mut self) -> &mut World {
+        self.world.register_component::<T>();
+        &mut self.world
+    }
+
+    pub fn add_system<T: 'static + System>(&mut self, system: T) -> &mut World {
+        self.world.add_system(system);
+        &mut self.world
+    }
+
+    pub fn add_component_to_entity<T: 'static + Component>(
+        &mut self,
+        entity_id: usize,
+        component: T,
+    ) -> &mut World {
+        self.world.add_component_to_entity(entity_id, component);
+        &mut self.world
     }
 }
