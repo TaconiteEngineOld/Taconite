f5e62f0d7eb5c061a973f983374d04f778905ffe
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-16 21:35:24 +0000

    Add a Shape component and Shapes enum

--- src/components.rs
+++ src/components.rs
@@ -7,11 +7,29 @@ pub struct Transform {
     pub rotation: Vector3,
 }
 
+pub enum Shapes {
+    Rectangle,
+}
+
+pub struct Shape {
+    pub shape: Shapes,
+    pub scale: Vector2,
+}
+
 impl Default for Transform {
     fn default() -> Self {
         Self {
             position: Vector3::new(0., 0., 0.),
             rotation: Vector3::new(0., 0., 0.),
+        }
+    }
+}
+
+impl Default for Shape {
+    fn default() -> Self {
+        Self {
+            shape: Shapes::Rectangle,
+            scale: Vector2::new(128., 128.),
         }
     }
 }
@@ -22,3 +40,4 @@ pub struct Sprite<'a> {
 
 impl Component for Transform {}
 impl Component for Sprite<'_> {}
+impl Component for Shape {}
