de20903dd24f738589012bf42ab747086ea59c9e
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-31 23:53:51 +0000

    Add RenderSystem trait that supports taking in a canvas

--- src/ecs/system.rs
+++ src/ecs/system.rs
@@ -1,5 +1,15 @@
 use super::entity_manager::{EntityIdAccessor, EntityManager};
+use sdl2::{render::Canvas, video::Window};
 
 pub trait System {
     fn update(&mut self, manager: &mut EntityManager, accessor: &mut EntityIdAccessor);
+}
+
+pub trait RenderSystem {
+    fn update(
+        &mut self,
+        manager: &mut EntityManager,
+        accessor: &mut EntityIdAccessor,
+        canvas: &mut Canvas<Window>,
+    );
 }
