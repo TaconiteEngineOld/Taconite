02452d6570495c6fdea2e9aa2c120cc21cc3617d
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-01 00:01:42 +0000

    Add new years code!

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod components;
 pub mod ecs;
 mod engine;
+mod new_year;
 mod renderer;
 mod types;
 
 pub use components::*;
 pub use ecs::*;
 pub use engine::*;
+pub use new_year::*;
 pub use types::*;
---
+++ src/new_year.rs
@@ -0,0 +1,3 @@
+pub mod new_year {
+    println!("This was wrote at the start of 2023! Happy new years!");
+}
