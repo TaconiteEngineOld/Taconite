9f64672086146ec945f71fdb4dce902c119230a0
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-01 00:07:33 +0000

    Change to a function in the module

--- src/new_year.rs
+++ src/new_year.rs
@@ -1,3 +1,5 @@
 pub mod new_year {
-    println!("This was wrote at the start of 2023! Happy new years!");
+    pub fn year() {
+        println!("This was wrote at the start of 2023! Happy new years!");
+    }
 }
