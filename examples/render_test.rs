7c405b3d5dac318df9051a32779e9cd7a7a1795e
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-02 20:34:41 +0000

    Update the Render example to render components

--- examples/render_test.rs
+++ examples/render_test.rs
@@ -1,27 +1,48 @@
 use log::info;
+use sdl2::rect::Rect;
 use taconite::*;
 
-struct RenderSys {}
+struct ComponentRenderer {}
 
-impl RenderSystem for RenderSys {
+struct MovementSystem {}
+
+impl System for MovementSystem {
+    fn update(&mut self, manager: &mut EntityManager, _accessor: &mut EntityIdAccessor) {
+        for transform in manager
+            .borrow_components_mut::<Transform>()
+            .unwrap()
+            .iter_mut()
+        {
+            transform.position.x += 1.;
+            transform.position.y += 1.;
+        }
+    }
+}
+
+impl RenderSystem for ComponentRenderer {
     fn update(
         &mut self,
-        _manager: &mut EntityManager,
+        manager: &mut EntityManager,
         _accessor: &mut EntityIdAccessor,
         canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
     ) {
-        let i = canvas.draw_color().r + 1;
-
-        info!("{}", i);
-
-        canvas.set_draw_color(Color {
-            r: i,
-            g: i,
-            b: i,
-            a: 255,
-        });
-
+        canvas.set_draw_color(Color::WHITE);
         canvas.clear();
+
+        canvas.set_draw_color(Color::RGB(10, 10, 10));
+
+        for transform in manager.borrow_components::<Transform>().unwrap().iter() {
+            canvas
+                .fill_rect(Rect::new(
+                    (transform.position.x - 5.0) as i32,
+                    (transform.position.y - 5.0) as i32,
+                    10,
+                    10,
+                ))
+                .expect("Failed to draw to the canvas.");
+        }
+
+        info!("Rendered frame.");
     }
 }
 
@@ -30,7 +51,48 @@ fn main() {
 
     let mut taconite = Taconite::default();
 
-    taconite.add_render_system(RenderSys {});
+    let entity = taconite.create_entity();
+    taconite.add_component_to_entity(
+        entity,
+        Transform::new(Vector3 {
+            x: 10.0,
+            y: 10.0,
+            z: 0.0,
+        }),
+    );
+
+    let entity = taconite.create_entity();
+    taconite.add_component_to_entity(
+        entity,
+        Transform::new(Vector3 {
+            x: 30.0,
+            y: 30.0,
+            z: 0.0,
+        }),
+    );
+
+    let entity = taconite.create_entity();
+    taconite.add_component_to_entity(
+        entity,
+        Transform::new(Vector3 {
+            x: 50.0,
+            y: 50.0,
+            z: 0.0,
+        }),
+    );
+
+    let entity = taconite.create_entity();
+    taconite.add_component_to_entity(
+        entity,
+        Transform::new(Vector3 {
+            x: 70.0,
+            y: 70.0,
+            z: 0.0,
+        }),
+    );
+
+    taconite.add_system(MovementSystem {});
+    taconite.add_render_system(ComponentRenderer {});
 
     taconite.start();
 }
