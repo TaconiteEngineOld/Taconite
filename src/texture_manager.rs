8d6f06981f766156aa9a4ff960b8d0d82f713791
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-04 23:25:37 +0000

    Add a default implementation of a TextureManager

--- src/texture_manager.rs
+++ src/texture_manager.rs
@@ -1,2 +1,20 @@
-#[derive(Default)]
-pub struct TextureManager {}
+use std::collections::HashMap;
+
+use sdl2::{
+    render::{Texture, TextureCreator},
+    video::WindowContext,
+};
+
+pub struct TextureManager<'a> {
+    texture_creator: &'a Option<TextureCreator<WindowContext>>,
+    textures: HashMap<String, Texture<'a>>,
+}
+
+impl Default for TextureManager<'_> {
+    fn default() -> Self {
+        Self {
+            texture_creator: &None,
+            textures: HashMap::default(),
+        }
+    }
+}
