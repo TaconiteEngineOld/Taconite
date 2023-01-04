0498c04d60bf3f92813a57a992bc975715d6cae8
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-04 22:01:18 +0000

    Add a way to set a texture creator as an Option in the EventHandler trait

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,14 +1,24 @@
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::Canvas, video::Window};
+use sdl2::{
+    event::Event,
+    image::InitFlag,
+    keyboard::Keycode,
+    pixels::Color,
+    render::{Canvas, TextureCreator},
+    video::{Window, WindowContext},
+};
 
 pub trait EventHandler {
     fn update(&mut self);
     fn draw(&mut self, canvas: &mut Canvas<Window>);
+    fn set_texture_creator(&mut self, context: Option<TextureCreator<WindowContext>>);
 }
 
 pub trait Renderer: EventHandler {
     fn start_window(&mut self) -> Result<(), String> {
         let sdl = sdl2::init()?;
         let video = sdl.video()?;
+
+        let _image_ctx = sdl2::image::init(InitFlag::PNG);
 
         let window = video
             .window("Temporary window", 800, 600)
@@ -22,6 +32,8 @@ pub trait Renderer: EventHandler {
             .present_vsync()
             .build()
             .map_err(|e| e.to_string())?;
+
+        self.set_texture_creator(Some(canvas.texture_creator()));
 
         canvas.set_draw_color(Color::RGB(46, 52, 64));
         canvas.clear();
