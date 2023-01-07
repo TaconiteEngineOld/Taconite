11f79d3be8e4f57650a272ad0d2d9949b72d067a
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-07 14:59:35 +0000

    Add some basic content to the TextureManager

--- src/texture_manager.rs
+++ src/texture_manager.rs
@@ -1,2 +1,11 @@
+use std::collections::HashMap;
 
+use sdl2::{
+    render::{Texture, TextureCreator},
+    video::WindowContext,
+};
 
+pub struct TextureManager<'a> {
+    pub texture_creator: TextureCreator<WindowContext>,
+    cache: HashMap<String, Texture<'a>>,
+}
