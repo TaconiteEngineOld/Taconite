d56e77f1b176784a147858f3f072ab5b581ae102
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-14 23:36:15 +0000

    Implement a load function that gets picturex

--- src/texture_manager.rs
+++ src/texture_manager.rs
@@ -11,3 +11,16 @@ pub struct TextureManager<'a> {
     texture_creator: TextureCreator<WindowContext>,
     cache: HashMap<String, Rc<Texture<'a>>>,
 }
+
+impl<'a> TextureManager<'a> {
+    pub fn load(&'a mut self, path: &str) -> Result<Rc<Texture>, String> {
+        self.cache.get(path).cloned().map_or_else(
+            || {
+                let resource = Rc::new(self.texture_creator.load_texture(path)?);
+                self.cache.insert(path.into(), resource.clone());
+                Ok(resource)
+            },
+            Ok,
+        )
+    }
+}
