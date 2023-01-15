ae84816eb50bcb82a4543b1c3a4c5cef148cf226
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-15 15:33:16 +0000

    Switch EventHandler to a struct

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,6 +1,16 @@
+use crate::World;
 use sdl2::{render::Canvas, video::Window};
+use std::sync::{Arc, Mutex};
 
-pub trait EventHandler {
-    fn update(&mut self);
-    fn draw(&mut self, canvas: &mut Canvas<Window>);
+pub struct EventHandler {
+    world: Arc<Mutex<World>>,
+}
+
+impl EventHandler {
+    fn update(&mut self) {
+        self.world.lock().unwrap().update();
+    }
+    fn draw(&mut self, canvas: &mut Canvas<Window>) {
+        self.world.lock().unwrap().update_render(canvas);
+    }
 }
