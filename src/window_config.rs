72cbd0a62477e38bc6df7595da239a8dc2074a35
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-15 23:28:14 +0000

    Update WindowConfig to have better fields

--- src/window_config.rs
+++ src/window_config.rs
@@ -1,17 +1,17 @@
-use crate::Vector2;
-
-pub struct WindowConfig {
-    pub name: String,
-    pub dimensions: Vector2,
+pub struct WindowConfig<'a> {
+    pub name: &'a str,
     pub vsync: bool,
+    pub width: i32,
+    pub height: i32,
 }
 
-impl WindowConfig {
-    pub fn new(name: String, vsync: bool, dimensions: Vector2) -> WindowConfig {
+impl<'a> WindowConfig<'a> {
+    pub fn new(name: &str, vsync: bool, width: i32, height: i32) -> WindowConfig {
         WindowConfig {
             name,
             vsync,
-            dimensions,
+            width,
+            height,
         }
     }
 }
