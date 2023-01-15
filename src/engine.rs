ed8f5d15b07fdf071a33bb4c5fe4358a4f446aff
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-15 15:51:04 +0000

    Make Taconite use an Arc and Mutex

--- src/engine.rs
+++ src/engine.rs
@@ -1,23 +1,23 @@
 use log::info;
 
+use std::sync::{Arc, Mutex};
+
 use crate::components::{Sprite, Transform};
 use crate::ecs::*;
-use crate::event_handler::EventHandler;
 use crate::renderer::*;
-use crate::texture_manager::TextureManager;
 use crate::Shape;
 
-use sdl2::{render::Canvas, video::Window};
-
 pub struct Taconite {
-    world: World,
+    world: Arc<Mutex<World>>,
     renderer: Option<Renderer>,
 }
 
 impl Default for Taconite {
     fn default() -> Self {
+        let world = Arc::new(Mutex::new(World::default()));
+
         let mut taconite = Self {
-            world: World::default(),
+            world: world.clone(),
             renderer: None,
         };
 
@@ -33,46 +33,47 @@ impl Default for Taconite {
 
 impl Taconite {
     pub fn create_entity(&mut self) -> usize {
-        self.world.create_entity()
+        self.world.lock().unwrap().create_entity()
     }
 
     pub fn remove_entity(&mut self, entity_id: usize) {
-        self.world.remove_entity(entity_id);
+        self.world.lock().unwrap().remove_entity(entity_id);
     }
 
-    pub fn register_component<T: 'static + Component>(&mut self) -> &mut World {
-        self.world.register_component::<T>();
-        &mut self.world
+    pub fn register_component<T: 'static + Component>(&mut self) {
+        self.world.lock().unwrap().register_component::<T>();
     }
 
-    pub fn add_system<T: 'static + System>(&mut self, system: T) -> &mut World {
-        self.world.add_system(system);
-        &mut self.world
+    pub fn add_system<T: 'static + System>(&mut self, system: T) {
+        self.world.lock().unwrap().add_system(system);
     }
 
-    pub fn add_render_system<T: 'static + RenderSystem>(&mut self, system: T) -> &mut World {
-        self.world.add_render_system(system);
-        &mut self.world
+    pub fn add_render_system<T: 'static + RenderSystem>(&mut self, system: T) {
+        self.world.lock().unwrap().add_render_system(system);
     }
 
     pub fn add_component_to_entity<T: 'static + Component>(
         &mut self,
         entity_id: usize,
         component: T,
-    ) -> &mut World {
-        self.world.add_component_to_entity(entity_id, component);
-        &mut self.world
+    ) {
+        self.world
+            .lock()
+            .unwrap()
+            .add_component_to_entity(entity_id, component);
     }
 
     pub fn update(&mut self) {
-        self.world.update();
+        self.world.lock().unwrap().update();
     }
 
-    pub fn start(&mut self) {
-        self.renderer
-            .as_mut()
-            .unwrap()
-            .start_window()
-            .expect("Failed to start the window.");
+    pub fn start(&mut self) -> Result<(), String> {
+        self.renderer = Some(Renderer::new(self.world.clone()));
+
+        self.begin()
+    }
+
+    fn begin(&mut self) -> Result<(), String> {
+        self.renderer.as_mut().unwrap().start_window()
     }
 }
