8b8011ad58fa0d2cd6914c801a14f6da428531d8
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-15 15:27:24 +0000

    Include math

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 mod components;
 pub mod ecs;
 mod engine;
+mod math;
 mod new_year;
 mod renderer;
 mod sdl_fronts;
 mod texture_manager;
-mod types;
 
 pub use components::*;
 pub use ecs::*;
 pub use engine::*;
+pub use math::*;
 pub use new_year::*;
 pub use sdl2::pixels::Color;
 pub use sdl_fronts::*;
 pub use texture_manager::*;
-pub use types::*;
