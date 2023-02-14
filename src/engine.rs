259dfc332f781feba6663930770219ba912d0b6b
TeaSpillStudios <teaspillstudios@proton.me>
2023-02-14 12:41:00 +0000

    Add some documentation

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,4 @@
 use log::{error, info};
-
 use std::sync::{Arc, Mutex};
 
 use crate::components::{Sprite, Transform};
@@ -7,6 +6,7 @@ use crate::renderer::*;
 use crate::Shape;
 use crate::{ecs::*, WindowConfig};
 
+/// The main struct that holds the engine.
 pub struct Taconite<'a> {
     world: Arc<Mutex<World>>,
     renderer: Option<Renderer<'a>>,
@@ -30,26 +30,38 @@ impl Default for Taconite<'_> {
 }
 
 impl Taconite<'_> {
+    /// Create a new entity.
+    /// This will create a new entity with the ECS system and return it's ID (a usize).
     pub fn create_entity(&mut self) -> usize {
         Mutex::lock(&self.world).unwrap().create_entity()
     }
 
+    /// Remove an entity.
+    /// This takes in the ID of the entity (a usize)
     pub fn remove_entity(&mut self, entity_id: usize) {
         Mutex::lock(&self.world).unwrap().remove_entity(entity_id);
     }
 
+    /// Register a new component.
+    /// This takes in a struct with `Component` implemeneted.
     pub fn register_component<T: 'static + Component>(&mut self) {
         Mutex::lock(&self.world).unwrap().register_component::<T>();
     }
 
+    /// Add a system to run every frame.
+    /// This takes in a sruct with `System` implemented.
     pub fn add_system<T: 'static + System>(&mut self, system: T) {
         Mutex::lock(&self.world).unwrap().add_system(system);
     }
 
+    /// Add a render system to run every frame (after normal systems are updated).
+    /// This takes in a struct with `RenderSystem` implemented.
     pub fn add_render_system<T: 'static + RenderSystem>(&mut self, system: T) {
         Mutex::lock(&self.world).unwrap().add_render_system(system);
     }
 
+    /// Add a component to an entity.
+    /// This takes in an ID (a usize) and a struct that implements `Component`.
     pub fn add_component_to_entity<T: 'static + Component>(
         &mut self,
         entity_id: usize,
@@ -60,10 +72,8 @@ impl Taconite<'_> {
             .add_component_to_entity(entity_id, component);
     }
 
-    pub fn update(&mut self) {
-        Mutex::lock(&self.world).unwrap().update();
-    }
-
+    /// Start the window and begin rendering and updating.
+    /// This takes in a `WindowConfig` and opens the window.
     pub fn start(&mut self, window_config: WindowConfig) {
         self.renderer = Some(Renderer::new(self.world.clone()));
 
