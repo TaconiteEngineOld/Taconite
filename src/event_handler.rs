85113dd2b0b2d02e06d4d51534fe23a58d95f99e
TeaSpillStudios <teaspillstudios@proton.me>
2023-02-14 13:03:20 +0000

    Add event handling directly into the `EventHandler` by giving it the `EventPump`

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,15 +1,16 @@
 use crate::World;
 use log::info;
-use sdl2::{event::Event, keyboard::Keycode, render::Canvas, video::Window};
+use sdl2::{event::Event, keyboard::Keycode, render::Canvas, video::Window, EventPump};
 use std::sync::{Arc, Mutex};
 
 pub struct EventHandler {
     world: Arc<Mutex<World>>,
+    pub(crate) event_pump: Option<EventPump>,
 }
 
 impl EventHandler {
-    pub fn new(world: Arc<Mutex<World>>) -> EventHandler {
-        EventHandler { world }
+    pub fn new(world: Arc<Mutex<World>>, event_pump: Option<EventPump>) -> EventHandler {
+        EventHandler { world, event_pump }
     }
 
     pub fn update(&mut self) {
@@ -20,14 +21,18 @@ impl EventHandler {
         self.world.lock().unwrap().update_render(canvas);
     }
 
-    pub(crate) fn event(&mut self, event: Event) -> bool {
-        matches!(
-            event,
-            Event::Quit { .. }
-                | Event::KeyDown {
+    pub(crate) fn handle_events(&mut self) -> bool {
+        for event in self.event_pump.as_mut().unwrap().poll_iter() {
+            return match event {
+                Event::Quit { .. } => true,
+                Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                }
-        )
+                } => true,
+                _ => false,
+            };
+        }
+
+        false
     }
 }
