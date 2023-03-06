171cde2863a4ba038097c39a2f1b31864b6a3f13
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-06 21:48:33 +0000

    Add information on the WindowConfig

--- src/window_config.rs
+++ src/window_config.rs
@@ -1,3 +1,6 @@
+/// The WindowConfig struct holds the information for how the window should be created.
+/// This is used later so that it is able to correctly inform the renderer on how to create the
+/// window when needed
 pub struct WindowConfig<'a> {
     pub name: &'a str,
     pub vsync: bool,
