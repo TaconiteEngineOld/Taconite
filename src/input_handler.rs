98dff8fccc245eb97cb37ba79ce67170b61a3896
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-06 22:01:47 +0000

    Add functions to handle inputs to the InputHandler

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -1,11 +1,30 @@
+use log::warn;
+
 #[derive(Default)]
 pub(crate) struct InputHandler {
-    pub keys: Vec<Key>,
+    keys: Vec<Key>,
 }
 
+#[allow(dead_code)] // TODO: Will be gone soon
+#[derive(PartialEq)]
 pub enum Key {
     W,
     A,
     S,
     D,
+}
+
+#[allow(dead_code)] // TODO: Will be gone soon
+impl InputHandler {
+    pub(crate) fn add_key(&mut self, key_variant: Key) {
+        if !self.keys.contains(&key_variant) {
+            self.keys.push(key_variant);
+        } else {
+            warn!("Key already pressed");
+        }
+    }
+
+    pub fn is_key_down(&self, key_variant: Key) -> bool {
+        self.keys.contains(&key_variant)
+    }
 }
