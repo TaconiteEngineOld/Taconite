03ce15a2b7a8562bc2534f83cee94f82371961a5
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-27 23:20:45 +0100

    Begin on work to making the EventHandler compatible with OpenGL

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,9 +1,17 @@
 // TODO: Upgrade to OpenGL
 
+use crate::renderer::Renderer;
 use crate::{input_handler::InputHandler, World};
+use glow::Context;
+use glow::HasContext;
+use glow::NativeVertexArray;
+use glow::Program;
+use glutin::event::WindowEvent;
 use glutin::{
     event::Event,
     event_loop::{ControlFlow, EventLoop},
+    window::Window,
+    ContextWrapper, PossiblyCurrent,
 };
 use log::{info, warn};
 use std::sync::{Arc, Mutex};
@@ -31,21 +39,45 @@ impl EventHandler {
         self.world.lock().unwrap().update_render(/*canvas*/);
     }
 
-    pub(crate) fn run(self) -> bool {
+    pub(crate) unsafe fn run(
+        self,
+        gl: Context,
+        window: ContextWrapper<PossiblyCurrent, glutin::window::Window>,
+    ) {
         self.event_loop.unwrap().run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Wait;
-
             match event {
                 Event::LoopDestroyed => {
-                    info!("Closing the window.");
+                    // Close the loop.
+
                     return;
                 }
-                _ => {
-                    warn!("Unhandled event");
+                Event::MainEventsCleared => {
+                    // Redraw the window.
+
+                    window.window().request_redraw();
                 }
+                Event::RedrawRequested(_) => {
+                    // Draw to the screen
+
+                    gl.clear(glow::COLOR_BUFFER_BIT);
+                    gl.draw_arrays(glow::TRIANGLES, 0, 3);
+                    window.swap_buffers().unwrap();
+                }
+                Event::WindowEvent { ref event, .. } => match event {
+                    WindowEvent::Resized(physical_size) => {
+                        // Resize the window
+
+                        window.resize(*physical_size);
+                    }
+                    WindowEvent::CloseRequested => {
+                        // Delete the program
+                        *control_flow = ControlFlow::Exit
+                    }
+                    _ => (),
+                },
+                _ => (),
             }
         });
-
-        true
     }
 }
