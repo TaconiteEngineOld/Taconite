613f25bccb47a064315420814a45a9678a62d0d4
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-04-16 01:10:42 +0100

    Add a fullscreen parameter to WindowConfig

--- src/window_config.rs
+++ src/window_config.rs
@@ -3,15 +3,17 @@
 /// window when needed
 pub struct WindowConfig<'a> {
     pub name: &'a str,
+    pub fullscreen: bool,
     pub vsync: bool,
     pub width: i32,
     pub height: i32,
 }
 
 impl<'a> WindowConfig<'a> {
-    pub fn new(name: &str, vsync: bool, width: i32, height: i32) -> WindowConfig {
+    pub fn new(name: &str, fullscreen: bool, vsync: bool, width: i32, height: i32) -> WindowConfig {
         WindowConfig {
             name,
+            fullscreen,
             vsync,
             width,
             height,
