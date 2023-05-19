5a523c056d31b024826d00a42fcfb1cecec97677
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-19 21:40:16 +0100

    Add `todo`s to the render system defaults

--- src/render_systems.rs
+++ src/render_systems.rs
@@ -1,5 +1,5 @@
-use crate::{ecs::RenderSystem, Shape, Shapes};
-use sdl2::rect::Rect;
+use crate::ecs::RenderSystem;
+// use sdl2::rect::Rect;
 
 pub struct ShapeRenderSystem {}
 
@@ -8,29 +8,7 @@ impl RenderSystem for ShapeRenderSystem {
         &mut self,
         manager: &mut crate::EntityManager,
         _accessor: &mut crate::EntityIdAccessor,
-        canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
     ) -> Option<()> {
-        let old_draw_color = canvas.draw_color();
-
-        for shape in manager.borrow_components::<Shape>()?.iter() {
-            match shape.shape {
-                Shapes::Rectangle => {
-                    canvas.set_draw_color(shape.color);
-
-                    canvas
-                        .fill_rect(Rect::new(
-                            shape.position.x as i32,
-                            shape.position.y as i32,
-                            shape.scale.x as u32,
-                            shape.scale.y as u32,
-                        ))
-                        .unwrap();
-                }
-            }
-        }
-
-        canvas.set_draw_color(old_draw_color);
-
-        Some(())
+        todo!("Setup the Shape Render System");
     }
 }
