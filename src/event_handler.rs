06f29f383933d895b4bd521596a820df796f9ce9
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-19 21:36:59 +0100

    Attempt to remove `sdl` from the event handler

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -2,21 +2,21 @@ use crate::{
     input_handler::{InputHandler, Key},
     World,
 };
-use sdl2::{event::Event, keyboard::Keycode, render::Canvas, video::Window, EventPump};
+// use sdl2::{event::Event, keyboard::Keycode, render::Canvas, video::Window, EventPump};
 use std::sync::{Arc, Mutex};
 
 #[allow(dead_code)]
 pub struct EventHandler {
     world: Arc<Mutex<World>>,
-    pub(crate) event_pump: Option<EventPump>,
+    // pub(crate) event_pump: Option<EventPump>,
     pub(crate) input_handler: InputHandler,
 }
 
 impl EventHandler {
-    pub fn new(world: Arc<Mutex<World>>, event_pump: Option<EventPump>) -> EventHandler {
+    pub fn new(world: Arc<Mutex<World>>) -> EventHandler {
         EventHandler {
             world,
-            event_pump,
+            // event_pump,
             input_handler: InputHandler::default(),
         }
     }
@@ -25,92 +25,13 @@ impl EventHandler {
         self.world.lock().unwrap().update(&self.input_handler);
     }
 
-    pub fn draw(&mut self, canvas: &mut Canvas<Window>, auto_clear: bool) {
-        if auto_clear {
-            canvas.clear();
-        }
+    pub fn draw(&mut self, auto_clear: bool) {
+        // if auto_clear {
+        //     canvas.clear();
+        // }
 
-        self.world.lock().unwrap().update_render(canvas);
+        self.world.lock().unwrap().update_render();
     }
 
-    // TODO: Use SDL keycodes.
-    pub(crate) fn handle_events(&mut self) -> bool {
-        let mut window_should_close = false;
-
-        for event in self.event_pump.as_mut().unwrap().poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => window_should_close = true,
-
-                // W down.
-                Event::KeyDown {
-                    keycode: Some(Keycode::W),
-                    ..
-                } => self.input_handler.add_key(Key::W),
-
-                // A down.
-                Event::KeyDown {
-                    keycode: Some(Keycode::A),
-                    ..
-                } => {
-                    self.input_handler.add_key(Key::A);
-                }
-
-                // S down.
-                Event::KeyDown {
-                    keycode: Some(Keycode::S),
-                    ..
-                } => {
-                    self.input_handler.add_key(Key::S);
-                }
-
-                // D down.
-                Event::KeyDown {
-                    keycode: Some(Keycode::D),
-                    ..
-                } => {
-                    self.input_handler.add_key(Key::D);
-                }
-
-                // W up.
-                Event::KeyUp {
-                    keycode: Some(Keycode::W),
-                    ..
-                } => {
-                    self.input_handler.remove_key(Key::W);
-                }
-
-                // A up.
-                Event::KeyUp {
-                    keycode: Some(Keycode::A),
-                    ..
-                } => {
-                    self.input_handler.remove_key(Key::A);
-                }
-
-                // S up.
-                Event::KeyUp {
-                    keycode: Some(Keycode::S),
-                    ..
-                } => {
-                    self.input_handler.remove_key(Key::S);
-                }
-
-                // D up.
-                Event::KeyUp {
-                    keycode: Some(Keycode::D),
-                    ..
-                } => {
-                    self.input_handler.remove_key(Key::D);
-                }
-
-                _ => {}
-            };
-        }
-
-        window_should_close
-    }
+    // NOTE: `handle_events` moved to `State`
 }
