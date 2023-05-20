102ab522ca615d27ed570eeaf6dea95b8f4fd678
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-20 20:48:58 +0100

    Add a basic `WindowStarter` and make a todo

---
+++ src/window_starter.rs
@@ -0,0 +1,10 @@
+use winit::window::Window;
+
+#[derive(Default)]
+pub struct WindowStarter();
+
+impl WindowStarter {
+    pub fn new_window() -> Result<(), Window> {
+        todo!("Make a window.");
+    }
+}
