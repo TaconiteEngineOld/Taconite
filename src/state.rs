817bcc191f4892376a0d137113cbfa4168fe5f4f
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-27 22:39:01 +0100

    Implement `render`

--- src/state.rs
+++ src/state.rs
@@ -113,8 +113,47 @@ impl State {
         // Nothing to update yet
     }
 
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!()
+    fn render(&mut self) -> Result<(), WindowError> {
+        let output = self
+            .surface
+            .get_current_texture()
+            .map_err(|_| Err::<(), WindowError>(WindowError::SurfaceFailure))?;
+
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 
     pub fn window_mut(&mut self) -> &mut Window {
