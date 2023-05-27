31b1acf02f9954a203513249a0f9af3482c4bd4c
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-27 16:59:07 +0100

    Allow dead code for the `WindowError` enum

--- src/window_starter.rs
+++ src/window_starter.rs
@@ -5,7 +5,9 @@ use crate::WindowConfig;
 #[derive(Default)]
 pub struct WindowStarter();
 
+// TODO: Remove allowing dead code
 #[derive(Debug, Error)]
+#[allow(dead_code)]
 pub enum WindowError {
     #[error("There was no GPU detected.")]
     NoGPU,
