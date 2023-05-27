ef29d10d5f76ff3199f422e2480e8da206662703
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-27 16:36:09 +0100

    Add extra error enums

--- src/window_starter.rs
+++ src/window_starter.rs
@@ -9,6 +9,10 @@ pub struct WindowStarter();
 pub enum WindowError {
     #[error("There was no GPU detected.")]
     NoGPU,
+    #[error("Creating a new window with Winit failed.")]
+    WindowFailure,
+    #[error("Creating a new wgpu surface failed.")]
+    SurfaceFailure,
 }
 
 impl WindowStarter {
