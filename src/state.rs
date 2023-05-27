cb78ec915b93b67763d8b9cfe9e1778a03b6c900
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-27 22:12:08 +0100

    Create the `new` function

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,7 @@
+#![allow(dead_code)]
 use winit::{event::*, window::Window};
+
+use crate::window_starter::WindowError;
 
 pub struct State {
     surface: wgpu::Surface,
@@ -11,19 +14,86 @@ pub struct State {
 
 impl State {
     // Some wgpu types require async
-    async fn new(window: Window) -> Self {
-        todo!()
+    pub async fn new(window: Window) -> Result<Self, WindowError> {
+        let size = window.inner_size();
+
+        // Instance is a handle to the GPU.
+        // Backends::all() -> Vulkan, Metal, DX12, Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            dx12_shader_compiler: Default::default(),
+        });
+
+        let surface =
+            unsafe { instance.create_surface(&window) }.map_err(|_| WindowError::SurfaceFailure)?;
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptionsBase {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .ok_or(WindowError::NoGPU)?;
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    // WebGL doesn't support all of wgpu's features, so if
+                    // we're building for the web we'll have to disable some.
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code in this tutorial assumes an Srgb surface texture. Using a different
+        // one will result all the colors comming out darker. If you want to support non
+        // Srgb surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.describe().srgb)
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+
+        Ok(Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        })
     }
 
     pub fn window(&self) -> &Window {
         &self.window
     }
 
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+    pub(crate) fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         todo!()
     }
 
-    fn input(&mut self, event: &WindowEvent) -> bool {
+    pub(crate) fn input(&mut self, event: &WindowEvent) -> bool {
         todo!()
     }
 
